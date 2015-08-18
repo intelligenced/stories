afa3a5ae671013d8f6f92d4b52948246f5eeb1c2
intelligenced <intelligenced@gmail.com>
2015-08-18 15:50:28 +0500

    Dependencies Localized

--- js/app.js
+++ js/app.js
@@ -57,52 +57,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       controller: 'StoryAddCtrl'
     }
   }
-})
-.state('tab.users', {
-  url: '/users',
-  views: {
-    'tab-users': {
-      templateUrl: 'templates/tab-users.html',
-      controller: 'UserCtrl',
-      resolve: {
-        night_users: function(Users) {
-          return Users.ball(); },
-        day_users:function(Users){
-          return Users.call();
-        }
-      }
-    }
-  }
-})
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+});
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/story');
