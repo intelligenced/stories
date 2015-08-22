90b5fd255edf289ee385d1f5055266c9f7eb1386
intelligenced <intelligenced@gmail.com>
2015-08-22 14:28:47 +0500

    storage ignored

--- js/controllers/track_manager.js
+++ js/controllers/track_manager.js
@@ -2,8 +2,20 @@ stories.controller('TrackCtrl', function($scope,$http) {
 
 
 
+$scope.addTrack = function(){
+
+  var track = $scope.add_track;
+  var link = './php/add_track.php';
+  $http.post(link, {track_name :track}).then(function (res){
+    var result=res.data
+            
+            console.log(result);
+          });
 
 
+
+
+}
 
 
 
