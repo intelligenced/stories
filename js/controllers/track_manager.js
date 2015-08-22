dc4a26bc3164d98dcb155255e9dbec8f103678cb
intelligenced <intelligenced@gmail.com>
2015-08-22 15:20:34 +0500

    track create/delete added

--- js/controllers/track_manager.js
+++ js/controllers/track_manager.js
@@ -1,10 +1,13 @@
 stories.controller('TrackCtrl', function($scope,$http) {
 
+
+$scope.refreshTracks = function(){
+
 $http.get("./php/get_tracks.php").then(function(response){
   $scope.tracks = response.data;
 
-});
-
+  });
+}//Refresh Tracks
 
 
 $scope.addTrack = function(){
@@ -13,12 +16,28 @@ $scope.addTrack = function(){
   var link = './php/add_track.php';
   $http.post(link, {track_name :track}).then(function (res){
     var result=res.data
-            
-            console.log(result);
+    $scope.refreshTracks();
+    console.log(result);
+
  });
-}
+}//Add Track
+
+$scope.deleteTrack = function(){
+
+  var track_id = $scope.tracks.selected.track_id;
+  console.log(track_id);
+
+  var link = './php/delete_track.php';
+  $http.post(link, {track_id :track_id}).then(function (res){
+    var result=res.data
+    $scope.refreshTracks();
+    console.log(result);
+
+ });
+}//Delete Track
 
 
+$scope.refreshTracks();
 
 
 
---
+++ php/delete_track.php
@@ -0,0 +1,31 @@
+<?php
+	require("config.php");
+
+    $postdata = file_get_contents("php://input");
+	if (isset($postdata)) {
+		$request = json_decode($postdata);
+		$track_id = $request->track_id;
+
+		echo $track_id;
+
+
+		$query = "DELETE FROM choices WHERE add_track=:track_id";
+		$sth   = $dbh->prepare($query);
+		$sth->execute(array(':track_id' => $track_id) );	
+
+
+		$query = "DELETE FROM choices WHERE check_track=:track_id";
+		$sth   = $dbh->prepare($query);
+		$sth->execute(array(':track_id' => $track_id) );	
+
+
+		$query = "DELETE FROM tracks WHERE track_id=:track_id";
+		$sth   = $dbh->prepare($query);
+		$sth->execute(array(':track_id' => $track_id) );	
+
+	
+
+		
+	}
+
+?>
