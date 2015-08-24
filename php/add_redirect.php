1f7db1cc7a2f6f5251cfaece1d0f52eb57c4a937
intelligenced <intelligenced@gmail.com>
2015-08-25 02:36:22 +0500

    ui overhaul

--- php/add_redirect.php
+++ php/add_redirect.php
@@ -6,9 +6,15 @@
    $postdata = file_get_contents("php://input");
 	if (isset($postdata)) {
 		$request = json_decode($postdata);
+
 		$parent = $request->parent;
 		$choice_text=$request->choice_text;
 		$choice_code=$request->choice_code;
+
+
+		if($choice_code==$parent){
+			echo "are you kidding me? ";
+		}else if($choice_code=="" || $parent=="" || $choice_text==""){ echo "type something,damn it";}else{
 
 		echo $choice_text;
 		echo $parent;
@@ -19,6 +25,8 @@
 
 /*
  		$add_choice = $dbh->exec("INSERT INTO choices(choice_text,choice_code,parent) VALUES('$choice_text','$new_code','$parent')")or die(print_r($dbh->errorInfo(), true));*/
+		}
+
 		}
 
 
